b62faa143eeaaad27ba86e65c09b2ba249c9d1bb
AndrewNikmel <119112789+AndrewNikmel@users.noreply.github.com>
2023-12-10 16:42:47 +0300

    Add files via upload

---
+++ sem4/src/Employee2.java
@@ -0,0 +1,34 @@
+import java.util.List;
+
+public class Employee2 {
+    private String nameEmployee;
+    private List<String> phoneNumber;
+    private float seniorityEmployee;
+
+    public Employee2(String nameEmployee, List<String> phoneNumber, float seniorityEmployee) {
+        this.nameEmployee = nameEmployee;
+        this.phoneNumber = phoneNumber;
+        this.seniorityEmployee = seniorityEmployee;
+    }
+
+    public String getNameEmployee() {
+        return nameEmployee;
+    }
+
+    public List<String> getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public float getSeniorityEmployee() {
+        return seniorityEmployee;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee2{" +
+                "nameEmployee='" + nameEmployee + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                ", seniorityEmployee=" + seniorityEmployee +
+                "}\n";
+    }
+}
